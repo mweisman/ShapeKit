e4a1612d41c0490be81794a59361e5f167637f1f
Michael Weisman <mweisman@gmail.com>
2011-03-04 13:59:38 -0800

    Build script now creates universal binaries of libraries

--- lib_src/build_libs.sh
+++ lib_src/build_libs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+mkdir -p ../ShapeKit/libs/
+
 echo "Downlaoding GEOS source..."
 curl -O "http://download.osgeo.org/geos/geos-3.2.2.tar.bz2"
 
@@ -11,9 +13,12 @@ cp build_ios geos-3.2.2
 
 echo "Building GEOS..."
 cd geos-3.2.2
-sh build_ios device
+sh build_ios -p $PWD/device_build device
 make clean
-sh build_ios simulator
+sh build_ios -p $PWD/simulator_build simulator
+lipo -arch i386 simulator_build/lib/libgeos.a -arch armv7 device_build/lib/libgeos.a -create -output ../../ShapeKit/libs/libgeos.a
+lipo -arch i386 simulator_build/lib/libgeos_c.a -arch armv7 device_build/lib/libgeos_c.a -create -output ../../ShapeKit/libs/libgeos_c.a
+cp -r simulator_build/include/* ../../ShapeKit/libs
 cd ..
 
 echo "Downloading PROJ4 source..."
@@ -27,9 +32,11 @@ cp build_ios proj-4.7.0
 
 echo "building PROJ4"
 cd proj-4.7.0
-sh build_ios device
+sh build_ios -p $PWD/device_build device
 make clean
-sh build_ios simulator
+sh build_ios -p $PWD/simulator_build simulator
+lipo -arch i386 simulator_build/lib/libproj.a -arch armv7 device_build/lib/libproj.a -create -output ../../ShapeKit/libs/libproj.a
+cp -r simulator_build/include/* ../../ShapeKit/libs
 cd ..
 
 echo "All Done!"
